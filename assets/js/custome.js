a87f71810ff6625825a9e6ef675e469af2bfc359
surajniyogi <surajniyogi@yahoo.in>
2020-05-21 00:18:35 +0530

    Scroll to top

--- assets/js/custome.js
+++ assets/js/custome.js
@@ -6,3 +6,11 @@ $(window).scroll(function(){
     else sticky.removeClass('fixed');
 });
 
+$(window).scroll(function(){
+    var sticky = $('.scrollUp'),
+        scroll = $(window).scrollTop();
+  
+    if (scroll >= 300) sticky.addClass('scrollToTop');
+    else sticky.removeClass('fixed');
+});
+
